dc62a4f876d908e45beb9cc5863709f08e952caf
wangkuiw <wkw020712@163.com>
2025-09-19 10:00:52 +0800

    跨域解决1

--- frontend/vue/src/services/api.js
+++ frontend/vue/src/services/api.js
@@ -12,43 +12,63 @@ const api = axios.create({
 export default {
   // 文章相关 API
   getPosts(params = {}) {
-    return api.get('/posts', { params });
+    return api.get('/posts', { params }).then(response => {
+      return response.data;
+    });
   },
 
   getPost(id) {
-    return api.get(`/posts?id=${id}`);
+    return api.get(`/posts?id=${id}`).then(response => {
+      return response.data;
+    });
   },
 
   createPost(post) {
-    return api.post('/posts', post);
+    return api.post('/posts', post).then(response => {
+      return response.data;
+    });
   },
 
   updatePost(post) {
-    return api.put(`/posts`, post);
+    return api.put(`/posts`, post).then(response => {
+      return response.data;
+    });
   },
 
   deletePost(id) {
-    return api.delete(`/posts`, { data: { id: id } });
+    return api.delete(`/posts`, { data: { id: id } }).then(response => {
+      return response.data;
+    });
   },
 
   // 分类相关 API
   getCategories() {
-    return api.get('/categories');
+    return api.get('/categories').then(response => {
+      return response.data;
+    });
   },
 
   getCategory(id) {
-    return api.get(`/categories?id=${id}`);
+    return api.get(`/categories?id=${id}`).then(response => {
+      return response.data;
+    });
   },
 
   createCategory(category) {
-    return api.post('/categories', category);
+    return api.post('/categories', category).then(response => {
+      return response.data;
+    });
   },
 
   updateCategory(category) {
-    return api.put(`/categories`, category);
+    return api.put(`/categories`, category).then(response => {
+      return response.data;
+    });
   },
 
   deleteCategory(id) {
-    return api.delete(`/categories`, { data: { id: id } });
+    return api.delete(`/categories`, { data: { id: id } }).then(response => {
+      return response.data;
+    });
   }
 };
